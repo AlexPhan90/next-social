8aaa2959e171b30fff8f8fd1b6ba428a5643cf80
quyen.phan <quyenphan.2010@gmail.com>
2024-08-08 19:54:20 +0700

    01:04:19

--- src/components/AddPost.tsx
+++ src/components/AddPost.tsx
@@ -1,5 +1,54 @@
+import Image from "next/image";
+
 const AddPost = () => {
-  return <div className="">AddPost</div>;
+  return (
+    <div className="p-4 bg-white rounded-lg flex gap-4 justify-between text-sm">
+      {/* AVATAR */}
+      <Image
+        src="https://images.pexels.com/photos/296282/pexels-photo-296282.jpeg?auto=compress&cs=tinsrgb&w=800&lazyload"
+        alt=""
+        width={48}
+        height={48}
+        className="w-12 h-12 object-cover rounded-full"
+      ></Image>
+      {/* POST */}
+      <div className="flex-1">
+        {/* TEXT INPUT */}
+        <div className="flex gap-4">
+          <textarea
+            placeholder="What's on your mind?"
+            className=" flex-1 bg-slate-100 rounded-lg p-2"
+          ></textarea>
+          <Image
+            src="/emoji.png"
+            alt=""
+            width={20}
+            height={20}
+            className="w-5 h-5 cursor-pointer self-end "
+          ></Image>
+        </div>
+        {/* POST OPTION */}
+        <div className="flex items-center gap-4 mt-4 text-gray-400">
+          <div className="flex items-center gap-2 cursor-pointer">
+            <Image src="/addimage.png" alt="" width={20} height={20}></Image>
+            Photo
+          </div>
+          <div className="flex items-center gap-2 cursor-pointer">
+            <Image src="/addVideo.png" alt="" width={20} height={20}></Image>
+            Video
+          </div>
+          <div className="flex items-center gap-2 cursor-pointer">
+            <Image src="/addevent.png" alt="" width={20} height={20}></Image>
+            Event
+          </div>
+          <div className="flex items-center gap-2 cursor-pointer">
+            <Image src="/poll.png" alt="" width={20} height={20}></Image>
+            Poll
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AddPost;
--- src/components/Stories.tsx
+++ src/components/Stories.tsx
@@ -1,7 +1,83 @@
+import Image from "next/image";
+
 const Stories = () => {
   return (
-    <div className=''>Stories</div>
-  )
-}
+    <div className="p-4 bg-white rounded-lg shadow-md overflow-scroll text-xs scrollbar-hide">
+      <div className="flex gap-8 w-max">
+        {/* STORIES */}
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://images.pexels.com/photos/296282/pexels-photo-296282.jpeg?auto=compress&cs=tinsrgb&w=800&lazyload"
+            alt=""
+            width={80}
+            height={80}
+            className="w-20 h-20 rounded-full ring-2"
+          />
+          <span className="font-medium">Ricky</span>
+        </div>
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://images.pexels.com/photos/296282/pexels-photo-296282.jpeg"
+            alt=""
+            width={80}
+            height={80}
+            className="w-20 h-20 rounded-full ring-2"
+          />
+          <span className="font-medium">Ricky</span>
+        </div>
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://images.pexels.com/photos/296282/pexels-photo-296282.jpeg"
+            alt=""
+            width={80}
+            height={80}
+            className="w-20 h-20 rounded-full ring-2"
+          />
+          <span className="font-medium">Ricky</span>
+        </div>
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://images.pexels.com/photos/296282/pexels-photo-296282.jpeg"
+            alt=""
+            width={80}
+            height={80}
+            className="w-20 h-20 rounded-full ring-2"
+          />
+          <span className="font-medium">Ricky</span>
+        </div>
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://images.pexels.com/photos/296282/pexels-photo-296282.jpeg"
+            alt=""
+            width={80}
+            height={80}
+            className="w-20 h-20 rounded-full ring-2"
+          />
+          <span className="font-medium">Ricky</span>
+        </div>
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://images.pexels.com/photos/296282/pexels-photo-296282.jpeg"
+            alt=""
+            width={80}
+            height={80}
+            className="w-20 h-20 rounded-full ring-2"
+          />
+          <span className="font-medium">Ricky</span>
+        </div>
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://images.pexels.com/photos/296282/pexels-photo-296282.jpeg"
+            alt=""
+            width={80}
+            height={80}
+            className="w-20 h-20 rounded-full ring-2"
+          />
+          <span className="font-medium">Ricky</span>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Stories
+export default Stories;
