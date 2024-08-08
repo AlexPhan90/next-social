3c52ced451e7f943bb096729e9412b593eaddcdf
quyen.phan <quyenphan.2010@gmail.com>
2024-08-08 11:54:27 +0700

    00:20:00

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+
+const Navbar = () => {
+  return (
+    <div className="">
+      {/* LEFT */}
+      <div className="">
+        <Link href="/">LAMASOCIAL</Link>
+      </div>
+      {/* CENTER */}
+      <div className="hidden"></div>
+      {/* RIGHT */}
+      <div className=""></div>
+    </div>
+  );
+};
+
+export default Navbar;
