6568fb462e8b002d38998e062cbe93e31c8fdf3a
quyen.phan <quyenphan.2010@gmail.com>
2024-08-10 09:12:11 +0700

    02:39:08

--- src/components/AddPost.tsx
+++ src/components/AddPost.tsx
@@ -1,12 +1,8 @@
+import prisma from "@/lib/client";
+import { auth } from "@clerk/nextjs/server";
 import Image from "next/image";
 
 const AddPost = () => {
-  const testAction = async () => {
-    try {
-    } catch (err) {
-      console.log(err);
-    }
-  };
   return (
     <div className="p-4 bg-white shadow-md rounded-lg flex gap-4 justify-between text-sm">
       {/* AVATAR */}
@@ -20,10 +16,11 @@ const AddPost = () => {
       {/* POST */}
       <div className="flex-1">
         {/* TEXT INPUT */}
-        <div className="flex gap-4">
+        <form action="" className="flex gap-4">
           <textarea
             placeholder="What's on your mind?"
             className=" flex-1 bg-slate-100 rounded-lg p-2"
+            name="desc"
           ></textarea>
           <Image
             src="/emoji.png"
@@ -32,7 +29,7 @@ const AddPost = () => {
             height={20}
             className="w-5 h-5 cursor-pointer self-end "
           ></Image>
-        </div>
+        </form>
         {/* POST OPTION */}
         <div className="flex items-center gap-4 mt-4 text-gray-400 flex-wrap">
           <div className="flex items-center gap-2 cursor-pointer">
