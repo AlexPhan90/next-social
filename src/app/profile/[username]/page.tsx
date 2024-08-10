33e58b89d3aa418c1094c9d8144aa4bc7753c245
quyen.phan <quyenphan.2010@gmail.com>
2024-08-10 11:49:00 +0700

    02:51:17

--- src/app/profile/[username]/page.tsx
+++ src/app/profile/[username]/page.tsx
@@ -2,8 +2,27 @@ import Feed from "@/components/feed/Feed";
 import LeftMenu from "@/components/leftMenu/LeftMenu";
 import RightMenu from "@/components/rightMenu/RightMenu";
 import Image from "next/image";
+import prisma from "@/lib/client";
+import { auth } from "@clerk/nextjs/server";
+import { notFound } from "next/navigation";
 
-const ProfilePage = () => {
+const ProfilePage = async ({ params }: { params: { username: string } }) => {
+  const username = params.username;
+  const user = await prisma.user.findFirst({
+    where: {
+      username,
+    },
+    include: {
+      _count: {
+        select: {
+          followers: true,
+          followings: true,
+          posts: true,
+        },
+      },
+    },
+  });
+  if (!user) return notFound();
   return (
     <div className="flex gap-6 pt-6">
       <div className="hidden xl:block w-[20%]">
