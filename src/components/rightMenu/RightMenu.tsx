110c9e194cdb6d78fcad5254514d94a8881427fd
quyen.phan <quyenphan.2010@gmail.com>
2024-08-10 17:26:03 +0700

    03:31:18

--- src/components/rightMenu/RightMenu.tsx
+++ src/components/rightMenu/RightMenu.tsx
@@ -4,14 +4,19 @@ import Birthdays from "./Birthdays";
 import FriendRequests from "./FriendRequests";
 import UserInfoCard from "./UserInfoCard";
 import UserMediaCard from "./UserMediaCard";
+import { Suspense } from "react";
 
 const RightMenu = ({ user }: { user?: User }) => {
   return (
     <div className="flex flex-col gap-6">
       {user ? (
         <>
-          <UserInfoCard user={user} />
-          <UserMediaCard user={user} />
+          <Suspense fallback="loading...">
+            <UserInfoCard user={user} />
+          </Suspense>
+          <Suspense fallback="loading...">
+            <UserMediaCard user={user} />
+          </Suspense>
         </>
       ) : null}
       <FriendRequests />
