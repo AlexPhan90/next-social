0cb24b6ca33340e88813a95d68df02c0fea301da
quyen.phan <quyenphan.2010@gmail.com>
2024-08-12 10:01:41 +0700

    commit

--- src/components/Stories.tsx
+++ src/components/Stories.tsx
@@ -1,80 +1,41 @@
+import prisma from "@/lib/client";
+import { auth } from "@clerk/nextjs/server";
 import Image from "next/image";
+import StoryList from "./StoryList";
 
-const Stories = () => {
+const Stories = async () => {
+  const { userId: currentUserId } = auth();
+
+  if (!currentUserId) return null;
+
+  const stories = await prisma.story.findMany({
+    where: {
+      expiresAt: {
+        gt: new Date(),
+      },
+      OR: [
+        {
+          user: {
+            followers: {
+              some: {
+                followerId: currentUserId,
+              },
+            },
+          },
+        },
+        {
+          userId: currentUserId,
+        },
+      ],
+    },
+    include: {
+      user: true,
+    },
+  });
   return (
     <div className="p-4 bg-white rounded-lg shadow-md overflow-scroll text-xs scrollbar-hide">
       <div className="flex gap-8 w-max">
-        {/* STORIES */}
-        <div className="flex flex-col items-center gap-2 cursor-pointer">
-          <Image
-            src="https://images.pexels.com/photos/296282/pexels-photo-296282.jpeg?auto=compress&cs=tinsrgb&w=800&lazyload"
-            alt=""
-            width={80}
-            height={80}
-            className="w-20 h-20 rounded-full ring-2"
-          />
-          <span className="font-medium">Ricky</span>
-        </div>
-        <div className="flex flex-col items-center gap-2 cursor-pointer">
-          <Image
-            src="https://images.pexels.com/photos/296282/pexels-photo-296282.jpeg"
-            alt=""
-            width={80}
-            height={80}
-            className="w-20 h-20 rounded-full ring-2"
-          />
-          <span className="font-medium">Ricky</span>
-        </div>
-        <div className="flex flex-col items-center gap-2 cursor-pointer">
-          <Image
-            src="https://images.pexels.com/photos/296282/pexels-photo-296282.jpeg"
-            alt=""
-            width={80}
-            height={80}
-            className="w-20 h-20 rounded-full ring-2"
-          />
-          <span className="font-medium">Ricky</span>
-        </div>
-        <div className="flex flex-col items-center gap-2 cursor-pointer">
-          <Image
-            src="https://images.pexels.com/photos/296282/pexels-photo-296282.jpeg"
-            alt=""
-            width={80}
-            height={80}
-            className="w-20 h-20 rounded-full ring-2"
-          />
-          <span className="font-medium">Ricky</span>
-        </div>
-        <div className="flex flex-col items-center gap-2 cursor-pointer">
-          <Image
-            src="https://images.pexels.com/photos/296282/pexels-photo-296282.jpeg"
-            alt=""
-            width={80}
-            height={80}
-            className="w-20 h-20 rounded-full ring-2"
-          />
-          <span className="font-medium">Ricky</span>
-        </div>
-        <div className="flex flex-col items-center gap-2 cursor-pointer">
-          <Image
-            src="https://images.pexels.com/photos/296282/pexels-photo-296282.jpeg"
-            alt=""
-            width={80}
-            height={80}
-            className="w-20 h-20 rounded-full ring-2"
-          />
-          <span className="font-medium">Ricky</span>
-        </div>
-        <div className="flex flex-col items-center gap-2 cursor-pointer">
-          <Image
-            src="https://images.pexels.com/photos/296282/pexels-photo-296282.jpeg"
-            alt=""
-            width={80}
-            height={80}
-            className="w-20 h-20 rounded-full ring-2"
-          />
-          <span className="font-medium">Ricky</span>
-        </div>
+        <StoryList stories={stories} userId={currentUserId} />
       </div>
     </div>
   );
